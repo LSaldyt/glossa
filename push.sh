1bff4d371dad891514d45e9e0af9946e564839b6
lucas <lucassaldyt@yahoo.com>
2016-05-22 07:44:37 -0600

    push.sh now runs run_tests.sh, and prompts the user to ask if they still want to push after the tests have been completed. Soon, I'll make it so that passing the tests is mandatory in order to push

--- push.sh
+++ push.sh
@@ -1,6 +1,17 @@
-echo Beginning commit
-git add *
-read -p "Enter commit message:" commitmessage
-git commit -m "$commitmessage"
-git push origin master
-echo Finished
+function push {
+    echo Beginning commit
+    git add *
+    read -p "Enter commit message:" commitmessage
+    git commit -m "$commitmessage"
+    git push origin master
+    echo Finished
+}
+
+echo Running Tests..
+./run_tests.sh
+read -p "Tests Completed. Are you satisfied with the results?" answer
+    case $answer in
+        [Yy]* ) push; break;;
+        [Nn]* ) echo Exiting..; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
--- tests/parse_test.cpp
+++ tests/parse_test.cpp
@@ -2,7 +2,7 @@
 #include "catch.hpp"
 #include "../src/Utilities/Parser/Parse.hpp"
 
-TEST_CASE( "Parsers return expected values")
+TEST_CASE( "Tokenizer acts as expected")
 {
     auto test_tokens = Parse::Tokens();
     test_tokens.push_back("hello");
