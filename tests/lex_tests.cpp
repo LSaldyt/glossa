5d4041dacc6d829c39fdd26491cdca8697df4cc0
lucas <lucassaldyt@yahoo.com>
2016-06-02 13:27:22 -0600

    Added unit tests for the lexer module, all of which pass :)

---
+++ tests/lex_tests.cpp
@@ -0,0 +1,37 @@
+#include "catch.hpp"
+#include "../src/Utilities/Lex/Lex.hpp"
+#include "../src/Utilities/Language.hpp"
+
+TEST_CASE("The lexer and language modules work")
+{
+    using namespace Parse;
+    using namespace Lex;
+
+    Terms keywords  = {"if", "then", "else", "is", "and", "not"};
+    Terms operators = {"+", "-", "/", "*"};
+
+    Lex::LanguageTermSets term_set;
+    term_set.push_back(std::make_tuple(keywords, "keyword"));
+    term_set.push_back(std::make_tuple(operators, "operator"));
+
+    Lex::LanguageParsers  parser_set;
+    parser_set.push_back(LanguageParser(Parse::digits, "int", "type"));
+    parser_set.push_back(LanguageParser(Parse::alphas, "", "identifier"));
+    Gen::SymbolicStatementParsers statement_parsers;
+
+    Lex::Language test_language(term_set, parser_set, statement_parsers);
+
+
+    SECTION("Lexing based on keywords, operators and whitespace")
+    {
+        auto test_sentence = "if 2 + 2 is 4 then 4 - 2 is 2";
+        auto tokens = lex(test_sentence, test_language);
+        REQUIRE(tokens[0].type     == "keyword");
+        REQUIRE(tokens[0].value    == "if");
+        REQUIRE(tokens[1].sub_type == "int");
+        REQUIRE(tokens[1].type     == "type");
+        REQUIRE(tokens[1].value    == "2");
+        REQUIRE(tokens[2].type     == "operator");
+        REQUIRE(tokens[2].value    == "+");
+    }
+}
