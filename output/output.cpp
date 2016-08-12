ee91fea6966b6439c51e6b339d4be313b7772904
LSaldyt <lucassaldyt@yahoo.com>
2016-08-11 19:58:25 -0600

    Updates example to show arbitrary complexity

--- output/output.cpp
+++ output/output.cpp
@@ -1,5 +1,6 @@
 #include "std/std.hpp"
 int main(){
 print("hello world");
-const auto foo = [=](auto x){ return x; };
+const auto bar = [=](auto y){print("Creating meta-function foo");const auto foo = [=](auto x){print("running foo with values:");print(x);print(y); return x; }; return foo; };
+bar(2);
 }
