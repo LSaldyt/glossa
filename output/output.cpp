45b528d9b5a16c2729054f5d7223b392ac1771b4
LSaldyt <lucassaldyt@yahoo.com>
2016-08-29 14:21:41 -0600

    Fixes some issues with initializing Result objects

--- output/output.cpp
+++ output/output.cpp
@@ -1,4 +1,6 @@
 #include "std/std.hpp"
 int main(){
-const auto f = [=](auto x){print(x); return x; };
+const auto f = [=](auto x){print(x); return x * 8; };
+const auto x = 5 * f(42);;
+const auto y = 3 + 2;
 }
--- src/Types/Result.hpp
+++ src/Types/Result.hpp
@@ -6,13 +6,14 @@ struct Result
     std::vector<T> consumed;
     std::vector<T> remaining;
     
-    Result(bool set_result,
-                   std::vector<T> set_consumed,
-                   std::vector<T> set_remaining) 
-        : result(set_result),
-          consumed(set_consumed),
-          remaining(set_consumed)
-    {}
+    Result(bool set_result=false,
+           std::vector<T> set_consumed=std::vector<T>(),
+           std::vector<T> set_remaining=std::vector<T>()) 
+    {
+        result    = set_result;
+        consumed  = set_consumed;
+        remaining = set_remaining;
+    }
 };
 
 
