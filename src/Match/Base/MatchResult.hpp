029a8d88a2b41f01369db5b7b472eda0ade5d622
LSaldyt <lucassaldyt@yahoo.com>
2016-08-29 19:46:08 -0600

    Converts remaining match functions -> polymorphic

--- src/Match/Base/MatchResult.hpp
+++ src/Match/Base/MatchResult.hpp
@@ -5,17 +5,4 @@
 namespace Match 
 {
     using MatchResult = Result<Term>; 
-        /*
-    struct MatchResult : public Consumed<Term>
-    {
-        Terms remaining;
-
-        MatchResult(bool set_result     = false,
-               Terms set_consumed  = Terms(),
-               Terms set_remaining = Terms())
-               : Consumed<Term>(set_result, set_consumed)
-        {
-            remaining = set_remaining;
-        }
-    };*/
 }
--- src/Match/Match.hpp
+++ src/Match/Match.hpp
@@ -1,13 +1,13 @@
 #pragma once
 #include "Base/Locale.hpp"
 #include "Base/Templates.hpp"
+#include "../Types/Result.hpp"
 
 namespace Match 
 {
 
-    /*
     template <typename T>
-    MatchResult<T> inOrderP (std::vector<std::function<MatchResult<T>(std::vector<T>)>> matchers)
+    Result<T> inOrder (std::vector<std::function<Result<T>(std::vector<T>)>> matchers)
     {
         return [matchers](const std::vector<T>& original_terms)
         {
@@ -24,14 +24,14 @@ namespace Match
                 }
                 else
                 {
-                    return MatchResult<T>(false, std::vector<T>(), original_terms);
+                    return Result<T>(false, std::vector<T>(), original_terms);
                 }
             }
-            return MatchResult<T>(true, consumed, terms);
+            return Result<T>(true, consumed, terms);
         };
     }
-    */
 
+    /*
     // Parse a list of functions in order, failing if any of them fail
     const auto inOrder = [](MatchFunctions matchers)
     {
@@ -57,44 +57,57 @@ namespace Match
         };
         return match;
     };
+    */
 
-    //Parse a single string, exactly
-    const auto just = [](std::string value)
+    const auto just = [](auto value)
     {
-        auto comparator = [value](Term term){ return value == term; };
-        return singleTemplate(comparator);
+        auto comparator = [value](decltype(value) term){ return value == term; };
+        return singleTemplate<decltype(value)>(comparator);
     };
 
-    //Parse a single string, exactly
     const auto startswith = [](std::string value)
     {
-        auto comparator = [value](Term term){ return std::string(term.begin(), term.begin() + value.size()) == value; };
-        return singleTemplate(comparator);
+        auto comparator = [value](Term term){ 
+            if (term.size() >= value.size())
+            {
+                return std::string(term.begin(), term.begin() + value.size()) == value; 
+            }
+            else
+            {
+                return false;
+            }
+        };
+        return singleTemplate<Term>(comparator);
     };
 
-    // Change the success of a matcher
-    const auto inverse = [](MatchFunction matcher)
+    template <typename T>
+    Result<T>
+    inverse
+    (std::function<Result<T>(std::vector<T>)> matcher)
     {
-        return matchTemplate([matcher](Terms terms)
+        return matchTemplate<T>([matcher](std::vector<T> terms)
         {
             auto result = matcher(terms);
             result.result = !result.result;
             return result;
         });
-    };
+    }
 
     // Attempt to parse any matcher from a list of matchers, failing only if all of the matchers fail, and passing if any of them pass
-    const auto anyOf = [](MatchFunctions functions)
+    template <typename T>
+    Result<T>
+    anyOf 
+    (std::vector<std::function<Result<T>(std::vector<T>)>> matchers)
     {
-        return matchTemplate([functions](Terms terms)
+        return matchTemplate<T>([matchers](std::vector<T> terms)
         {
-            auto result = MatchResult(false, Terms(), terms);
-            for (auto function : functions)
+            auto result = Result<T>(false, std::vector<T>(), terms);
+            for (auto matcher : matchers)
             {
-                auto func_result = function(terms);
-                if(func_result.result)
+                auto match_result = matcher(terms);
+                if(match_result.result)
                 {
-                    result = func_result;
+                    result = match_result;
                     break;
                 }
             }
@@ -102,83 +115,70 @@ namespace Match
         });
     };
 
+
     //Parse all matchers from a list of matchers, passing only if all of them pass
-    const auto allOf = [](MatchFunctions functions)
+    template <typename T>
+    Result<T>
+    allOf
+    (std::vector<std::function<Result<T>(std::vector<T>)>> matchers)
     {
-        return matchTemplate([functions](Terms terms)
+        return matchTemplate<T>([matchers](std::vector<T> terms)
         {
-            auto result = MatchResult(false, Terms(), terms);
-            for (auto function : functions)
+            auto result = Result<T>(false, std::vector<T>(), terms);
+            for (auto matcher : matchers)
             {
-                auto func_result = function(terms);
-                if(!func_result.result)
+                auto match_result = matcher(terms);
+                if(!match_result.result)
                 {
-                    result = MatchResult(false, Terms(), terms);
+                    result = Result<T>(false, std::vector<T>(), terms);
                     break;
                 }
                 else
                 {
-                    result = func_result;
+                    result = match_result;
                 }
             }
             return result;
         });
     };
 
-    // Convert a list of strings to a list of just(string)s
-    const auto justFrom = [](std::vector<std::string> strings)
-    {
-        auto functions = MatchFunctions();
-        functions.reserve(strings.size());
-        for (auto s : strings)
-        {
-            functions.push_back(just(s));
-        }
-        return functions;
-    };
-
     // Parse any term
-    const auto wildcard = singleTemplate([](Term t) { return true; } );
-
-    // Parse a term that is a subset of another term
-    const auto subsetOf = [](std::string symbols)
+    template <typename T>
+    Result<T>
+    wildcard
+    ()
     {
-        return singleTemplate([symbols](Term term)
+        return singleTemplate<T>([](T t){return true;});
+    }
+
+    // Takes a matcher and parses it repeatedly, never fails
+    template <typename T>
+    Result<T>
+    many
+    (std::function<Result<T>(std::vector<T>)> matcher)
+    {
+        return multiTemplate([matcher](std::vector<T> terms)
         {
-            for(auto c : term)
-            {
-                if(symbols.empty() || (symbols.find_first_not_of(c) == std::string::npos))
-                {
-                    return false;
-                }
-            }
-            return true;
+            auto result = matcher(terms);
+            return Consumed<T>(result.result, result.consumed);
         });
     };
 
-    // Takes a matcher and parses it repeatedly, never fails
-    const auto many = [](MatchFunction matcher)
+    template <typename T>
+    Result<T>
+    sepBy
+    (std::function<Result<T>(std::vector<T>)> sep, std::function<Result<T>(std::vector<T>)> val=wildcard)
     {
-        Consumer consumer = [matcher](Terms terms)
-        {
-            auto result = matcher(terms);
-            return Consumed<Term>(result.result, result.consumed);
-        };
-        return multiTemplate(consumer);
-    };
-
-    const auto sepBy = [](MatchFunction sep, MatchFunction val=wildcard)
-    {
-        return inOrder({
+        return inOrder<T>({
         val,
-        many(inOrder({sep, val}))
+        many<T>(inOrder<T>({sep, val}))
         });
     };
 
     //All of these are pretty self explanatory, they check a Term to see if it is a particular group of characters
-    const auto digits = singleTemplate(is_digits);
-    const auto alphas = singleTemplate(is_alphas);
-    const auto puncts = singleTemplate(is_puncts);
-    const auto uppers = singleTemplate(is_uppers);
-    const auto lowers = singleTemplate(is_lowers);
+    const auto digits = singleTemplate<std::string>(is_digits);
+    const auto alphas = singleTemplate<std::string>(is_alphas);
+    const auto puncts = singleTemplate<std::string>(is_puncts);
+    const auto uppers = singleTemplate<std::string>(is_uppers);
+    const auto lowers = singleTemplate<std::string>(is_lowers);
 }
