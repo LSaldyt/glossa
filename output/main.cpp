98ac1ba0e5ac04877918c6151d2a6310c50938a0
LSaldyt <lucassaldyt@yahoo.com>
2016-11-30 09:12:53 -0700

    Adds vector literals, element access, cosmetics

--- output/main.cpp
+++ output/main.cpp
@@ -1,8 +1,12 @@
 #include "main.hpp"
 int main(int argc, char ** argv) 
 { 
-if ( true  ) { auto x = 10  ;  ; ;
-while ( x  > 10  ) { print ( x )  ; ;
-x = x  - 1  ;  ; ;}  ; ;
-return 0  ;  ; ;} else { return 1  ;  ; ;}  ; 
+auto l = std::vector<decltype( 3  )>({ 3 ,15 ,1 ,7 ,2 ,4 ,5 ,3 ,12 })    
+;auto a = l [ 0 ]    
+;print ( "Running Main"s )  
+;if ( true  ) { print ( "Beginning while loop"s )  ;
+auto x = 10   ;
+while ( x  > 0  ) { print ( x )  ;
+x = x  - 1   ;}  ;
+return 0   ;} else { return 1   ;}  
 } 
