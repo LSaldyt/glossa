8d70c081b49c8bb61ec23b681ec35fdb45e08935
LSaldyt <lucassaldyt@yahoo.com>
2016-12-05 10:49:12 -0700

    Adds benchmarks for sorting algorithm

--- output/main.cpp
+++ output/main.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char ** argv) 
 { 
 print ( "Running main"s )  ;
-auto l = std::vector<Object>({ 3 ,2 ,12 ,9 ,4 ,68 ,17 })    ;
+auto l = std::vector<Object>({ 3 ,2 ,12 ,9 ,4 ,68 ,17 ,1 ,2 ,3 ,4 ,5 ,6 ,12 ,9 ,8 ,7 ,6 ,5 ,4 ,743 })    ;
 print ( "Unsorte"s )  ;
 for (auto item : l  ) { print ( item )  ;}  ;
 l = sort ( l )    ;
