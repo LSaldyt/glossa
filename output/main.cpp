a1c733cfdf71d1c8ed18db52248af3d1a27d7249
LSaldyt <lucassaldyt@yahoo.com>
2016-12-05 11:00:10 -0700

    Updates benchmark to perform 100,000 iterations

--- output/main.cpp
+++ output/main.cpp
@@ -3,10 +3,5 @@
 int main(int argc, char ** argv) 
 { 
 print ( "Running main"s )  ;
-auto l = std::vector<Object>({ 3 ,2 ,12 ,9 ,4 ,68 ,17 ,1 ,2 ,3 ,4 ,5 ,6 ,12 ,9 ,8 ,7 ,6 ,5 ,4 ,743 })    ;
-print ( "Unsorted"s )  ;
-for (auto item : l  ) { print ( item )  ;}  ;
-l = sort ( l )    ;
-print ( "Sorted"s )  ;
-for (auto item : l  ) { print ( item )  ;}  ;
+for (auto i : range ( 100000 )   ) { auto l = sort ( std::vector<Object>({ 3 ,2 ,12 ,9 ,4 ,68 ,17 ,1 ,2 ,3 ,4 ,5 ,6 ,12 ,9 ,8 ,7 ,6 ,5 ,4 ,743 })  )    ;}  ;
 } 
