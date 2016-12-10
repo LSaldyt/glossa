183d1abcc17891f4e30533e8500dd90d53808cf7
LSaldyt <lucassaldyt@yahoo.com>
2016-12-10 13:37:35 -0700

    Adds list comprehensions

--- output/main.cpp
+++ output/main.cpp
@@ -1,7 +1,9 @@
 #include "main.hpp"
-// No source for non-main functions 
 int main(int argc, char ** argv) 
 { 
-print ( "Running main"s )  ;
-auto l = sort ( std::vector<Object>({ 3 ,2 ,12 ,9 ,4 ,68 ,17 ,1 ,2 ,3 ,4 ,5 ,6 ,12 ,9 ,8 ,7 ,6 ,5 ,4 ,743 })  )    ;
+print ( "Hello World"s )  ;
+auto l = std::vector<Object>({ 1 ,2 ,3 })    ;
+for (auto i : l  ) { print ( i )  ;}  ;
+auto d = listComp( l  , std::function<Object(const Object&)>( [&](const auto& i ) { return 2  * i  ; } ) );    ;
+for (auto i : d  ) { print ( i )  ;}  ;
 } 
--- std/vectorhelpers.hpp
+++ std/vectorhelpers.hpp
@@ -1,10 +1,13 @@
 #pragma once
 #include <vector>
+#include <functional>
 
-std::vector<int> range(int a);
+using std::vector;
+
+vector<int> range(int a);
 
 template <typename T>
-std::vector<T> operator+(std::vector<T> lhs, const std::vector<T>& rhs)
+vector<T> operator+(vector<T> lhs, const vector<T>& rhs)
 {
     lhs.insert(lhs.end(), rhs.begin(), rhs.end());
     return lhs;
@@ -14,5 +17,22 @@ template <typename T>
 std::size_t len(const T& t)
 {
     return t.size();
+}
+
+template <typename T, typename U>
+vector<U> listComp(const vector<T>&                  v, 
+                   std::function<U   (const T&)> trans, 
+                   std::function<bool(const T&)> keep  = [](const T&){ return true; })
+{
+    vector<U> result;
+    result.reserve(v.size());
+    for (const auto& elem : v)
+    {
+        if (keep(elem))
+        {
+            result.push_back(trans(elem));
+        }
+    }
+    return result;
 }
 
