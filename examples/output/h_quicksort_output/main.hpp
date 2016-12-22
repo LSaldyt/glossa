f9272dd17ffbcb3f4c93fa65adc54ae57e87d886
LSaldyt <lucassaldyt@yahoo.com>
2016-12-21 17:49:21 -0700

    Adds initial quicksort pattern matching

--- examples/output/h_quicksort_output/main.hpp
+++ examples/output/h_quicksort_output/main.hpp
@@ -1,4 +1,4 @@
 #include "../std/std.hpp"
-template < typename T_l> auto empty ( T_l l) { return l  == std::list<Object>({ })   ;}  
-template < typename T_l> auto quicksort ( T_l l) { return empty  ;}  
-template < typename T_main> auto l ( T_main main) { return 0  ;}  
+template < typename T_l> auto quicksort ( T_l l) { std::vector<Object> __func_args__({ l}); /* Pattern-matched overloads: */ if (__func_args__ == std::vector<Object>({ std::list<Object>({ }) })) { return std::list<Object>({ })    ; } /* Default body: */ return std::list<Object>({ })   ;}  
+/*No Representation*/ 
+// No main declaration required  
