ad3419b4ebb5a8b94475dd3c2459770790d79808
LSaldyt <lucassaldyt@yahoo.com>
2016-12-28 22:33:03 -0700

    Adds obscure changes to print function

--- examples/output/porting_output/main.py
+++ examples/output/porting_output/main.py
@@ -1,2 +1,5 @@
-if __name__ == "__main__" : 
-     print( "Hello World"   )   
+from sys import *  
+def fileprint ( ) : 
+     print( "Hello World"   , file= sys . stderr   )   
+if __name__ == "__main__" :
+     print( "Hello World"   , end='')   
