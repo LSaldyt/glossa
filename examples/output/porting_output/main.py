ebe9ffc74cef98fb18daa256f148f9a0e2e9b7c4
LSaldyt <lucassaldyt@yahoo.com>
2016-12-29 11:18:53 -0700

    Adds exception traceback conversions

--- examples/output/porting_output/main.py
+++ examples/output/porting_output/main.py
@@ -2,6 +2,6 @@ from sys import *
 def fileprint ( ) : 
      print( "Hello World"   , file= sys . stderr   )   
 def exception ( ) : 
-     raise ValueError  ( "Weird value"  )   
+     raise ValueError  ( "Weird value"  ) .with_traceback()   
 if __name__ == "__main__" :
      print( "Hello World"   , end='')   
