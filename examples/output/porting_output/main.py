a4bda8a67f6e8d63688f91d503c42b915bf635a1
LSaldyt <lucassaldyt@yahoo.com>
2016-12-29 11:06:37 -0700

    Adds basic exception conversions for porting

--- examples/output/porting_output/main.py
+++ examples/output/porting_output/main.py
@@ -1,5 +1,7 @@
 from sys import *  
 def fileprint ( ) : 
      print( "Hello World"   , file= sys . stderr   )   
+def exception ( ) : 
+     raise ValueError  ( "Weird value"  )   
 if __name__ == "__main__" :
      print( "Hello World"   , end='')   
