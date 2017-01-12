150f44cdb0ecdb88ee9492c4c714995b64deddc1
LSaldyt <lucassaldyt@yahoo.com>
2017-01-12 09:24:55 -0700

    Adds grammar improvements to remaining languages

--- examples/output/fortran_output/main.cpp
+++
@@ -1,5 +0,0 @@
-#include "main.hpp"
-int main(int argc, char ** argv)
-{
-    
-}
--- structure_grammar.py
+++ structure_grammar.py
@@ -2,36 +2,20 @@
 import subprocess, shutil, time, sys, os
 from pprint import pprint
 
+def dirnames(directory):
+    names = []
+    for dirpath, dnames, filenames in os.walk(directory):
+        names.extend(dnames)
+        break
+    return names
+
+
 def filenames(directory):
     names = []
     for dirpath, dnames, filenames in os.walk(directory):
-        for filename in filenames:
-            filepath = os.path.join(dirpath, filename)
-            names.append(filename)
+        names.extend(filenames)
+        break
     return names
-
-"""
-def build_any_of(filename, content):
-    with open(filename, 'w') as anyof:
-        anyof.write('anyOf %s\n0' % ' '.join(content))
-
-def structure_grammar(language):
-    types     = ['statement', 'value']
-    directory = 'languages/' + language + '/grammar/'
-    core = []
-    for t in types:
-        type_dir  = '%ss/' % t
-        names     = [type_dir + name for name in filenames(directory + type_dir)]
-        type_list = ['link %s' % name for name in names]
-        build_any_of(directory + t, type_list)
-        core += names
-
-    with open(directory + 'core', 'w') as corefile:
-        corefile.write('\n'.join(core))
-
-if __name__ == '__main__':
-    structure_grammar('python3_grammar_update')
-"""
 
 def structure_grammar(language):
     directory = 'languages/' + language + '/grammar/'
@@ -41,6 +25,8 @@ def structure_grammar(language):
         corefile.write('\n'.join(corefiles))
 
 if __name__ == '__main__':
-    structure_grammar('python3')
-    structure_grammar('python2')
-    structure_grammar('haskell')
+    excluded = ['symboltables', 'cython', 'cython3']
+    languages = dirnames('languages/')
+    for language in languages:
+        if language not in excluded:
+            structure_grammar(language)
