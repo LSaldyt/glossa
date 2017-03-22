0f46dded74aca6cb72b248346f5b385a998525c2
LSaldyt <lucassaldyt@gmail.com>
2017-03-22 16:19:11 -0600

    Adds python3 constructors for fortran -> python

---
+++ examples/pyfortran/main.f90
@@ -0,0 +1,14 @@
+PROGRAM hello 
+    WRITE(*,*) 'Hello World'
+    REAL :: Area_circle
+    IF (1 > 0) THEN
+        WRITE(*,*) 'DANG'
+    ELSE IF (2 < 3) THEN
+        WRITE(*,*) 'DANG'
+    ELSE
+        WRITE(*,*) 'DANG'
+    END IF
+
+    DO WHILE (1 > 2)
+    END DO
+END PROGRAM 
